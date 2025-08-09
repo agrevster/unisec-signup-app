b961ef1ec8e969ad6afc5bc2798233d0242c625c
alpharant <evie.putz@gmail.com>
2025-08-08 21:33:51 -0500

    signup form

---
+++ signup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+exec 3>&1
+
+FORM_DATA=$(dialog --title "User Details" --form "Enter User Details" 15 50 0 \
+"Name: "  1 1 "" 1 10 20 0 \
+"Email: " 2 1 "" 2 10 20 0 \
+2>&1 1>&3)
+
+exec 3>&-
+
+EXIT_CODE=$?
+if [ $EXIT_CODE -ne 0 ]; then
+  echo "User cancelled (Exit Code: $EXIT_CODE). Exiting."
+  exit 1
+fi
+
+TSV_LINE=$(echo "$FORM_DATA" | paste -sd '\t' -)
+
+echo "$TSV_LINE" >> test.tsv
