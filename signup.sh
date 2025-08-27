b6d582d613e93c20b2239e1c1e08960a78ee8ecc
Evie Putz <evie@evieputz.fyi>
2025-08-27 16:34:46 -0500

    cs social hour

--- signup.sh
+++ signup.sh
@@ -9,7 +9,9 @@ while true; do
     FORM_DATA=$(dialog --stdout --title "UNISEC: User Details" \
       --form "Enter User Details" 15 50 0 \
       "Name: "  1 1 ""  1 10 20 0 \
-      "Email: " 2 1 ""  2 10 20 0)
+      "Email: " 2 1 ""  2 10 20 0 \
+      "Year: " 3 1 "" 3 10 20 0 \
+      "What days of week/times work best for you?" 4 1 "" 5 0 50 0)
 
     if [ $? -ne 0 ]; then
       break
@@ -17,8 +19,10 @@ while true; do
 
     NAME=$(echo "$FORM_DATA" | sed -n 1p)
     EMAIL=$(echo "$FORM_DATA" | sed -n 2p)
+    YEAR=$(echo "$FORM_DATA" | sed -n 3p)
+    TIME=$(echo "$FORM_DATA" | sed -n 4p)
 
-    if [ -z "$NAME" ] || [ -z "$EMAIL" ]; then
+    if [ -z "$NAME" ] || [ -z "$EMAIL" ] || [ -z "$YEAR" ] || [ -z "$TIME" ]; then
       dialog --title "Error" --msgbox "All fields are required. Please fill them in." 6 50
       continue
     fi
